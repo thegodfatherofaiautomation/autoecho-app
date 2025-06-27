b4ecf98eee213771975b17127d486dc0e01d532a
thegodfatherofaiautomation <thegodfatherofaiautomation@gmail.com>
2025-06-28 05:39:14 +0800

    Update app.py
    added
    import os
    
    if __name__ == "__main__":
        port = int(os.environ.get("PORT", 5000))
        app.run(host="0.0.0.0", port=port, debug=False)

--- app.py
+++ app.py
@@ -28,5 +28,9 @@ def upload():
 def transcript():
     return render_template("transcript.html", filename="example.mp3", tier="Free", transcript_file="output/example.txt")
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
+
